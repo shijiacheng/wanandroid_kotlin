e5dabf9ef9f9a0fcd8c9f47b99cab1511388b5fa
shijiacheng <iot_shijiacheng@163.com>
2019-02-23 23:39:29 +0800

    登录页面添加

---
+++ app/src/main/java/com/shijc/wanandroidkotlin/ui/account/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.shijc.wanandroidkotlin.ui.account
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.shijc.wanandroidkotlin.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
